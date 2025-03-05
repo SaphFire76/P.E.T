e9e61c32dfa7947c650723dc7def735820d3e908
Mobeen88 <mobeenali1238@gmail.com>
2025-03-05 18:43:33 +0000

    i

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const openBtn = document.getElementById("settingBtn");
-const modal_container = document.getElementById("settingModal");
-const closeBtn = document.querySelector(".close");
+document.addEventListener("DOMContentLoaded", function() {
+    const openBtn = document.getElementById("settingBtn");
+    const modal_container = document.getElementById("settingModal");
+    const closeBtn = document.querySelector(".close");
 
 
-modal_container.style.display = "none";
-
-openBtn.addEventListener("click", () => {
-    modal_container.style.display = "flex";
-});
-
-closeBtn.addEventListener("click", () => {
-    console.log("closing")
     modal_container.style.display = "none";
-});
 
-window.addEventListener("click", (e) =>{
-    if(e.target == open) {
+    openBtn.addEventListener("click", () => {
+        modal_container.style.display = "flex";
+    });
+
+    closeBtn.addEventListener("click", () => {
         modal_container.style.display = "none";
-    }
+    });
+
+    window.addEventListener("click", (e) =>{
+        if(e.target == open) {
+            modal_container.style.display = "none";
+        }
+    });
 });
