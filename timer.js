8e75179854ee82231847355dc12a5198ed0bfa21
Abrar <araha002@gold.ac.uk>
2025-03-09 02:46:25 +0000

    Working countdown timer, Timer precise to 10ms, Settings icon animation

--- timer.js
+++ timer.js
@@ -1,28 +1,35 @@
 let countdown;
 
 function startTimer() {
-    let timeLeft = 25 * 60;
+    var timerDuration= 3000;
+    let timeLeft = timerDuration;
     const timerDisplay = document.getElementById("timer");
     const button = document.querySelector(".catbtn")
+    var circle= document.getElementById('countdown_svg').children[0];
 
     button.disabled = true;
 
     clearInterval(countdown);
 
     countdown = setInterval(() => {
-        let minutes = Math.floor(timeLeft / 60);
-        let seconds = timeLeft % 60;
+        timeLeft--;
+        let minutes = Math.floor(timeLeft / 6000);
+        let seconds = Math.floor((timeLeft % 6000)/100);
+        var dashOffset= -(959-959*(timeLeft/timerDuration));
 
         timerDisplay.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+        circle.style.strokeDashoffset= dashOffset.toString();
 
         if (timeLeft <= 0) {    
             clearInterval(countdown);
-            timerDisplay.textContent = "00:00"; 
-            alert("Time's up!");
+            timerDisplay.textContent = "25:00";
+            circle.style.strokeDashoffset= '0';
+            //alert("Time's up!");
 
             button.disabled = false;
-        } else {
-            timeLeft--;
         }
-    }, 1000);
+        //  else {
+        //     timeLeft--;
+        // }
+    }, 10);
 }
